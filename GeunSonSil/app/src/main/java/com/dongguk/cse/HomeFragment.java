8a9abe3f735b6c702c5b7e910f7e9fc4f9fd072e
Jeonghwan Lee <ljh931111@gmail.com>
2019-12-14 20:35:16 +0900

    그래프 기능 구현 및 랭킹, 비교정보 분석 기능 구현

--- GeunSonSil/app/src/main/java/com/dongguk/cse/HomeFragment.java
+++ GeunSonSil/app/src/main/java/com/dongguk/cse/HomeFragment.java
@@ -21,20 +21,34 @@ public class HomeFragment extends Fragment {
         DEF,
         HP,
         MP,
-        WALK
+        WALK,
+        LEVEL
     }
 
     private View rootView;
-    private TextView holder_atk, holder_def, holder_hp, holder_mp, holder_walk;
+    private TextView holder_atk, holder_def, holder_hp, holder_mp, holder_walk, holder_level;
 
     private volatile int walk_counter = 0;
-
+    private volatile int level = 1;
     private BroadcastReceiver myRecv = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.e("Walking Detected", intent.toString());
             walk_counter++;
-            setValueOf(Integer.toString(walk_counter), TEXT_TYPE.WALK);
+            setValueOf(Integer.toString(50-walk_counter), TEXT_TYPE.WALK);
+            if(walk_counter==50)
+            {
+                walk_counter = 0;
+                level  ++;
+                setValueOf("Lv."+Integer.toString(level), TEXT_TYPE.LEVEL);
+
+            }
+
+            setValueOf(Integer.toString(level*60), TEXT_TYPE.ATK);
+            setValueOf(Integer.toString(level*50), TEXT_TYPE.DEF);
+            setValueOf(Integer.toString(level*200)+"/"+Integer.toString(level*200), TEXT_TYPE.MP);
+            setValueOf(Integer.toString(level*300)+"/"+Integer.toString(level*300), TEXT_TYPE.HP);
+
         }
     };
 
@@ -45,8 +59,16 @@ public class HomeFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
         initView();
+        int atk = 60;
+        int def = 50;
+        int hp = 300;
+        int mp = 200;
 
-        setValueOf("sddfsdf", TEXT_TYPE.ATK);
+
+        setValueOf(Integer.toString(atk), TEXT_TYPE.ATK);
+        setValueOf(Integer.toString(def), TEXT_TYPE.DEF);
+        setValueOf(Integer.toString(mp)+"/"+Integer.toString(mp), TEXT_TYPE.MP);
+        setValueOf(Integer.toString(hp)+"/"+Integer.toString(hp), TEXT_TYPE.HP);
 
         return rootView;
     }
@@ -57,6 +79,7 @@ public class HomeFragment extends Fragment {
         this.holder_hp = rootView.findViewById(R.id.holder_hp);
         this.holder_mp = rootView.findViewById(R.id.holder_mp);
         this.holder_walk = rootView.findViewById(R.id.holder_walk);
+        this.holder_level = rootView.findViewById(R.id.holder_level);
 
         getActivity().registerReceiver(myRecv, new IntentFilter("test_ggg_intent"));
     }
@@ -83,6 +106,9 @@ public class HomeFragment extends Fragment {
                 break;
             case WALK:
                 this.holder_walk.setText(value);
+                break;
+            case LEVEL:
+                this.holder_level.setText(value);
                 break;
         }
     }
--- GeunSonSil/app/src/main/java/com/dongguk/cse/LogFragment.java
+++ GeunSonSil/app/src/main/java/com/dongguk/cse/LogFragment.java
@@ -1,25 +1,82 @@
 package com.dongguk.cse;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by HP on 2018-01-02.
  */
-
 public class LogFragment extends Fragment {
-
     private View rootView;
+    private LineChart lineChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         rootView = inflater.inflate(R.layout.fragment_log, container, false);
+        lineChart = (LineChart) rootView.findViewById(R.id.chart);
+
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(0, 0));
+        entries.add(new Entry(3, 200));
+        entries.add(new Entry(6, 500));
+        entries.add(new Entry(9, 1400));
+        entries.add(new Entry(12, 0));
+
+        LineDataSet lineDataSet = new LineDataSet(entries, "걸음 수");
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setCircleRadius(6);
+        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setCircleColorHole(Color.BLUE);
+        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+        lineDataSet.setDrawHighlightIndicators(false);
+        lineDataSet.setDrawValues(false);
+
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.enableGridDashedLine(8, 24, 0);
+
+        YAxis yLAxis = lineChart.getAxisLeft();
+        yLAxis.setTextColor(Color.BLACK);
+
+        YAxis yRAxis = lineChart.getAxisRight();
+        yRAxis.setDrawLabels(false);
+        yRAxis.setDrawAxisLine(false);
+        yRAxis.setDrawGridLines(false);
+
+        Description description = new Description();
+        description.setText("");
+
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription(description);
+        lineChart.animateY(2000, Easing.EasingOption.EaseInCubic);
+        lineChart.invalidate();
+
+
 
         return rootView;
     }
